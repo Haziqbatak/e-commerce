544f148787d0c936eed58ef635bed90f01a66cb9
Haziqbatak <haziqimtiyaz17@gmail.com>
2024-04-24 13:14:21 +0700

    new

--- resources/views/layouts/include/navbar.blade.php
+++ resources/views/layouts/include/navbar.blade.php
@@ -217,10 +217,14 @@
                     </li>
 
                     <li>
-                        <a class="dropdown-item d-flex align-items-center" href="#">
-                            <i class="bi bi-box-arrow-right"></i>
-                            <span>Sign Out</span>
-                        </a>
+                        <form action="{{ route('logout') }}" method="post">
+                            @csrf
+                            <button class="dropdown-item d-flex align-items-center" type="submit">
+                                <i class="bi bi-box-arrow-right"></i>
+                                <span>Sign Out</span>
+                            </button>
+                        </form>
+                        <script src="{{ asset('dashboard/assets/js/main.js') }}"></script>
                     </li>
 
                 </ul><!-- End Profile Dropdown Items -->
