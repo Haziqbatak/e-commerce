fa8446ebc3ff1da520e347859b884c9841e9e784
Haziqbatak <haziqimtiyaz17@gmail.com>
2024-04-26 14:22:24 +0700

    new change

--- resources/views/layouts/include/sidebar/admin.blade.php
+++ resources/views/layouts/include/sidebar/admin.blade.php
@@ -12,20 +12,20 @@
         <li class="nav-item">
             <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse"
                 href="#">
-                <i class="bi bi-menu-button-wide"></i><span>Components</span><i
+                <i class="bi bi-menu-button-wide"></i><span>Data Products</span><i
                     class="bi bi-chevron-down ms-auto"></i>
             </a>
             <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
                 <li>
                     <a href="components-alerts.html">
-                        <i class="bi bi-circle"></i><span>Alerts</span>
+                        <i class="bi bi-circle"></i><span>Category</span>
                     </a>
                 </li>
                 <li>
                     <a href="components-accordion.html">
-                        <i class="bi bi-circle"></i><span>Accordion</span>
+                        <i class="bi bi-circle"></i><span>Product</span>
                     </a>
-                </li>
+                {{-- </li>
                 <li>
                     <a href="components-badges.html">
                         <i class="bi bi-circle"></i><span>Badges</span>
@@ -85,9 +85,10 @@
                     <a href="components-tooltips.html">
                         <i class="bi bi-circle"></i><span>Tooltips</span>
                     </a>
-                </li>
+                </li> --}}
             </ul>
-        </li><!-- End Components Nav -->
+        </li>
+        <!-- End Components Nav -->
 
         
 
