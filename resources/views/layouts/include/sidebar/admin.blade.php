c9cd9b5a69916e8aabb1da59d3d34e719a95c965
Haziqbatak <haziqimtiyaz17@gmail.com>
2024-04-26 15:02:35 +0700

    new main

--- resources/views/layouts/include/sidebar/admin.blade.php
+++ resources/views/layouts/include/sidebar/admin.blade.php
@@ -3,28 +3,33 @@
     <ul class="sidebar-nav" id="sidebar-nav">
 
         <li class="nav-item">
-            <a class="nav-link " href="{{ route('admindashboard') }}">
+            <a class="nav-link " href="{{ route('admin.dashboard') }}">
                 <i class="bi bi-grid"></i>
                 <span>Dashboard</span>
             </a>
         </li><!-- End Dashboard Nav -->
 
         <li class="nav-item">
-            <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse"
+            <a class="nav-link {{ request()->routeIs('admin.category.*') ? '' : 'collapsed' }}" data-bs-target="#components-nav" data-bs-toggle="collapse"
                 href="#">
-                <i class="bi bi-menu-button-wide"></i><span>Data Products</span><i
+                <i class="bi bi-menu-button-wide"></i><span>Product</span><i
                     class="bi bi-chevron-down ms-auto"></i>
             </a>
-            <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+            <ul id="components-nav" class="nav-content collapse {{ request()->routeIs('admin.category.*') ? 'show' : '' }}" data-bs-parent="#sidebar-nav">
                 <li>
-                    <a href="components-alerts.html">
+                    <a href="{{ route('admin.category.index') }}" class="{{ request()->routeIs('admin.category.index') ? 'active' : '' }}">
                         <i class="bi bi-circle"></i><span>Category</span>
                     </a>
                 </li>
+            </ul>
+            <ul id="components-nav" class="nav-content collapse {{ request()->routeIs('admin.category.*') ? 'show' : '' }}" data-bs-parent="#sidebar-nav">
                 <li>
-                    <a href="components-accordion.html">
+                    <a href="#">
                         <i class="bi bi-circle"></i><span>Product</span>
                     </a>
+                </li>
+            </ul>
+        </li>
                 {{-- </li>
                 <li>
                     <a href="components-badges.html">
@@ -86,8 +91,6 @@
                         <i class="bi bi-circle"></i><span>Tooltips</span>
                     </a>
                 </li> --}}
-            </ul>
-        </li>
         <!-- End Components Nav -->
 
         
